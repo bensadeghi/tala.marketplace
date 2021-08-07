ba3f8e9fcc913954ce5ba96cf9ab4fdaf52be04b
Ben Sadeghi <ben.sadeghi1@gmail.com>
2021-08-07 11:58:45 +0800

    setup gh-pages action

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  // Use the prefix in production and not development.
+  assetPrefix: isProd ? '/tala.marketplace/' : ''
 }
